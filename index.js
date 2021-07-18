f79275aa9850a9194bf66ba777f7807c6a2f93e2
presh900 <tt@presh900.33mail.com>
2021-07-18 14:14:39 +0100

    Fully working version. Speed control and synchronization

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
 const  variables  = document.getElementsByClassName('count-up');
 
 for (let i = 0; i < variables.length; i++) {
-    console.log(variables[i].innerHTML);
 
     var number = variables[i].innerHTML;
-    
-    countup(number,i);
-
-    // result = countup(number);
-
-    // document.getElementsByClassName('count-up')[i].innerHTML = result;
+    var speed = ((variables[i].getAttribute('data-speed') || 100)/number)*100;
+    countup(number,i,speed);
     
 }
 
-function countup(number,i) {
+function countup(number,i,speed) {
     var start = 0;
-    console.log("number is "+number+" and start is "+start);
     const x_count =setInterval(()=>{
         if(start==number){
-            console.log(start+" is equals "+number);
             clearInterval(x_count);
             return;
         }
@@ -26,6 +19,5 @@ function countup(number,i) {
         start+=1;
         document.getElementsByClassName('count-up')[i].innerHTML= start;
 
-
-    },100)
+    },speed)
 }
